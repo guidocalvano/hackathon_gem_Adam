9f16b4fd279dd8fbc64be36c18446beb14d6b98b
Guido Sales Calvano <guidocalvano@yahoo.com>
2018-11-21 14:11:22 +0100

    WIP data import run script, default configuration and a README on how to run things

--- README.md
+++ README.md
@@ -4,15 +4,39 @@
 
 ## Installation
 
+It is highly recommended (and assumed throughout this readme) that you create and activate a virtual environment before you start:
+
+```shell
+python3 -m venv env
+source env/bin/activate
+```
+
+To exit the virtual environment
+```shell 
+deactivate
+```
+
+To install all dependencies (after activating the virtual environment):
+```shell
+pip install -r requirements.txt
+```
+
 
 ## Deployment
 
 
 ## Running
 
-
 ### Data import
 
+To run the default data import run: 
+```shell
+source env/bin/activate
+
+python run_data_import.py.
+```
+
+To see any changable parameters for data import see defaults.json
 
 ### Machine Learning
 
--- run_data_import.py
+++ run_data_import.py
@@ -1,7 +1,20 @@
-import DataImporter from DataImporter
+from Configuration import Configuration
+from DataImporter import DataImporter
 
-config = Configuration.get()
+config = Configuration.load()
 
 description = config.parse_arguments()
 
-DataImporter.importData(**config["data_importer"])
+di = DataImporter()
+di.convert_to_standard_resolution(
+    config["data_importer"]["unstandardized_photos"],
+    config["data_importer"]["standardized_photos"],
+    config["data_importer"]["output_image_dimensions"],
+    config["data_importer"]["loading_config"]
+)
+
+DataImporter.load_from_cache(
+    config["data_importer"]["cache_file_path"],
+    config["data_description_file_path"],
+    config["standardized_photos"]
+)
