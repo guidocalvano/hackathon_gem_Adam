e8af62249d575213deac2807e76be17fafa2c457
Guido Sales Calvano <guidocalvano@yahoo.com>
2018-11-23 12:56:22 +0100

    Learning code simplified

--- Learning.py
+++ Learning.py
@@ -1,19 +1,32 @@
 from Configuration import Configuration
 from DataImporter import DataImporter
 
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import AveragePooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Input
+from keras.layers import Reshape
+
+from keras.callbacks import EarlyStopping
+from keras.models import Model
+from sklearn.metrics import confusion_matrix
+from keras.utils import to_categorical
+import numpy as np
+
+
 class Learning:
 
     @staticmethod
-    def simple_binary_classification(self, data_set):
-
-        di = DataImporter()
-
-        data_set = di.import_all_data(data_description_file_path, standardized_photos_file_path)
+    def simple_binary_classification(data_set, epochs=10, batch_size=320):
 
         inputs = Input(shape=data_set["training"][0].shape[1:])
         layer_1 = Conv2D(32, 5, padding='valid', activation='relu', kernel_initializer='glorot_normal')(inputs)
         layer_2 = MaxPooling2D(pool_size=(2, 2))(layer_1)
-        layer_3 = Conv2D(32, 5, border_mode='valid', activation='relu', kernel_initializer='glorot_normal')(layer_2)
+        layer_3 = Conv2D(32, 5, padding='valid', activation='relu', kernel_initializer='glorot_normal')(layer_2)
         layer_4 = MaxPooling2D(pool_size=(2, 2))(layer_3)
         layer_5 = Flatten()(layer_4)
         outputs = Dense(units=1, activation='sigmoid', name='prediction', kernel_initializer='glorot_normal')(layer_5)
@@ -26,24 +39,87 @@ class Learning:
                                        patience=0,
                                        verbose=0, mode='auto')
 
-        results = model.fit(ds["training"][0],
-                            ds["training"][1],
-                            validation_data=ds["validation"],
-                            epochs=10,
-                            batch_size=320,
+        results = model.fit(data_set["training"][0],
+                            data_set["training"][1].label_clean_int,
+                            validation_data=(data_set["validation"][0], data_set["validation"][1].label_clean_int),
+                            epochs=epochs,
+                            batch_size=batch_size,
                             callbacks=[early_stopping])
 
         validation_accuracy = results.history["val_acc"][-1]
 
-        evaluation = model.evaluate(ds["test"][0], ds["test"][1])
+        evaluation = model.evaluate(data_set["test"][0], data_set["test"][1].label_clean_int)
 
         return {
             "validation": validation_accuracy,
-            "test": evaluation
+            "test": evaluation[1]
         }
 
-    def categorical_classification(self):
-        pass
+    @staticmethod
+    def simple_categorical_classification(data_set, epochs = 10, batch_size = 320):
 
-    def crow_score_regression(self):
-        pass
+        inputs = Input(shape=data_set["training"][0].shape[1:])
+        layer_1 = Conv2D(32, 5, padding='valid', activation='relu', kernel_initializer='glorot_normal')(inputs)
+        layer_2 = MaxPooling2D(pool_size=(2, 2))(layer_1)
+        layer_3 = Conv2D(32, 5, padding='valid', activation='relu', kernel_initializer='glorot_normal')(layer_2)
+        layer_4 = MaxPooling2D(pool_size=(2, 2))(layer_3)
+        layer_5 = Flatten()(layer_4)
+        outputs = Dense(units=(np.max(data_set["training"][1].label_type_int) + 1), activation='softmax', name='prediction', kernel_initializer='glorot_normal')(layer_5)
+
+        model = Model(inputs=inputs, outputs=[outputs])
+
+        model.compile(optimizer='adam', loss=['categorical_crossentropy'], metrics=['accuracy'])
+
+        early_stopping = EarlyStopping(monitor='val_acc',
+        patience = 0,
+        verbose = 0, mode = 'auto')
+
+        results = model.fit(data_set["training"][0],
+            to_categorical(data_set["training"][1].label_type_int),
+            validation_data = (data_set["validation"][0], to_categorical(data_set["validation"][1].label_type_int)),
+            epochs = epochs,
+            batch_size = batch_size,
+            callbacks = [early_stopping])
+
+        validation_accuracy = results.history["val_acc"][-1]
+
+        evaluation = model.evaluate(data_set["test"][0], to_categorical(data_set["test"][1].label_type_int))
+
+        return {
+            "validation": validation_accuracy,
+            "test": evaluation[1]
+        }
+
+    @staticmethod
+    def simple_crow_score_regression(data_set, epochs=10, batch_size=320):
+        inputs = Input(shape=data_set["training"][0].shape[1:])
+        layer_1 = Conv2D(32, 5, padding='valid', activation='relu', kernel_initializer='glorot_normal')(inputs)
+        layer_2 = MaxPooling2D(pool_size=(2, 2))(layer_1)
+        layer_3 = Conv2D(32, 5, padding='valid', activation='relu', kernel_initializer='glorot_normal')(layer_2)
+        layer_4 = MaxPooling2D(pool_size=(2, 2))(layer_3)
+        layer_5 = Flatten()(layer_4)
+        outputs = Dense(units=1, activation='relu', name='prediction', kernel_initializer='glorot_normal')(layer_5)
+
+        model = Model(inputs=inputs, outputs=[outputs])
+
+        model.compile(optimizer='adam', loss=['mean_squared_error'], metrics=['mse'])
+
+        early_stopping = EarlyStopping(monitor='val_acc',
+                                       patience=0,
+                                       verbose=0, mode='auto')
+
+        results = model.fit(data_set["training"][0],
+                            data_set["training"][1].label_crow_score_int,
+                            validation_data=(data_set["validation"][0], data_set["validation"][1].label_crow_score_int),
+                            epochs=epochs,
+                            batch_size=batch_size,
+                            callbacks=[early_stopping])
+
+        validation_accuracy = results.history["val_mean_squared_error"][-1]
+
+        evaluation = model.evaluate(data_set["test"][0], data_set["test"][1].label_crow_score_int)
+
+        return {
+            "validation": validation_accuracy,
+            "test": evaluation[1]
+        }
--- tests/test_Learning.py
+++ tests/test_Learning.py
@@ -1,5 +1,11 @@
 import unittest
 from Learning import Learning
+import os
+import os.path
+from DataImporter import DataImporter
+import pandas as pd
+import numpy as np
+import shutil
 
 
 class test_Learning(unittest.TestCase):
@@ -7,6 +13,9 @@ class test_Learning(unittest.TestCase):
     def setUp(self):
         self.unstandardized_photos_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources', 'learning', 'unstandardized_photos')
         self.photos_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources', 'learning', 'photos')
+        self.test_photos_csv_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources', 'learning', 'test_photos.csv')
+
+        self.dataImporter = DataImporter()
 
         self.dataImporter.convert_to_standard_resolution(
             self.unstandardized_photos_path,
@@ -22,10 +31,29 @@ class test_Learning(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def simple_binary_classification(self):
+    def test_simple_binary_classification(self):
 
         data_set = self.dataImporter.import_all_data(self.test_photos_csv_file_path, self.photos_path)
 
-        results = Learning.simple_binary_classification(data_set)
+        results = Learning.simple_binary_classification(data_set, epochs=1, batch_size=2)
 
-        pass  # for breakpoints
+        self.assertTrue(np.isfinite(results["validation"]))
+        self.assertTrue(np.isfinite(results["test"]))
+
+    def test_simple_categorical_classification(self):
+
+        data_set = self.dataImporter.import_all_data(self.test_photos_csv_file_path, self.photos_path)
+
+        results = Learning.simple_categorical_classification(data_set, epochs=1, batch_size=2)
+
+        self.assertTrue(np.isfinite(results["validation"]))
+        self.assertTrue(np.isfinite(results["test"]))
+
+    def test_simple_crow_score_regression(self):
+
+        data_set = self.dataImporter.import_all_data(self.test_photos_csv_file_path, self.photos_path)
+
+        results = Learning.simple_crow_score_regression(data_set, epochs=1, batch_size=2)
+
+        self.assertTrue(np.isfinite(results["validation"]))
+        self.assertTrue(np.isfinite(results["test"]))
